a19976685120084c7105b55934ad44ee6ead5a7a
Manpreet "Mannie" Singh <raevox@gmail.com>
2016-10-27 19:34:48 -0400

    Refactor large portion of the project -- finished home page, need to do the rest

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -8,9 +8,11 @@ $(document).ready(function () {
     headers: { 'X_CSRF_TOKEN': 'N5JGY3RWWTW51XZTYVSN' },
     success: function success(data) {
       var htmlString = '';
-      data.blogs.forEach(function (blog, index) {
-        htmlString += '\n        <div class="col-md-12 blog-post-' + (index + 1) + ' blog-post">\n            <div class="row">\n              <div id="blog-post-title" class="blog-title">' + blog.title + '</div>\n              <div id="blog-post-time" class="blog-time">' + moment(blog.posted).fromNow() + '</div>\n              <div id="blog-post-text" class="blog-text comment more">' + blog.description + '</div>\n            </div>\n        </div>';
+
+      data.blogs.forEach(function (blog) {
+        htmlString += '\n        <div class="blog-post">\n          <h1>' + blog.title + '</h1>\n          <h2>' + moment(blog.posted).fromNow() + '</h2>\n          <p>' + blog.description + '<p>\n        </div>\n        <hr />';
       });
+
       $('#blog-data-goes-here').html(htmlString);
     }
   });
