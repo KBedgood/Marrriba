b9c787d2e621d7d14b9c16269f9c51277772272b
Manpreet "Mannie" Singh <raevox@gmail.com>
2016-10-31 07:34:24 -0400

    Style and sync login form

--- scripts/app.js
+++ scripts/app.js
@@ -48,7 +48,7 @@ function handleUserButton(event) {
 }
 
 function displayLoginError(message) {
-  if (loginError.hasClass('invisible')) loginError.removeClass('invisible');
+  loginError.show();
   loginError.find('span').html(message);
 }
 
